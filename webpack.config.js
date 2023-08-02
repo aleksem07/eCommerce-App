9fb706c4aee66daf9157a1afebf7d0052d3bdb07
aleksem07 <aleksem07@gmail.com>
2023-08-02 11:05:44 +0300

    fix: del favicon

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const ESLintPlugin = require("eslint-webpack-plugin");
 const StylelintPlugin = require("stylelint-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
 
 const mode = process.env.NODE_ENV || "development";
 const devMode = mode === "development";
@@ -39,9 +38,6 @@ module.exports = {
     }),
     new ESLintPlugin({ extensions: "ts" }),
 
-    new CopyPlugin({
-      patterns: [{ from: "static", to: "./" }],
-    }),
     new StylelintPlugin({
       configFile: "./.stylelintrc",
       files: "**/*.scss",
