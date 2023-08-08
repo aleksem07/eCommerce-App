ee355aab2cf726bc64ce002946f54c8603437719
NikitaStarmoussov <nikta.starmousov@gmail.com>
2023-08-09 00:26:30 +0500

    refactor: util validate function

--- src/utils/input-validation.ts
+++ src/utils/input-validation.ts
@@ -1,18 +1,14 @@
 export function validateForm(data: string, type: string) {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
-  switch (data) {
+  let isEmailValid, isPasswordValid;
+
+  switch (type) {
     case "email":
-      const isEmailValid = data.trim().match(emailRegex);
+      isEmailValid = data.trim().match(emailRegex);
       return isEmailValid;
     case "password":
-      const isPasswordValid = data.trim().match(passwordRegex);
+      isPasswordValid = data.trim().match(passwordRegex);
       return isPasswordValid;
   }
-
-  // Password validation
-  const isPasswordValid = password.match(passwordRegex);
-
-  // Return validation result
-  return isEmailValid && isPasswordValid;
 }
