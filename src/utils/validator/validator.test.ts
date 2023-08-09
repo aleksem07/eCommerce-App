3a070b5cee7f1a9f32aabc9cb6e74f44b41a04fb
NikitaStarmoussov <nikta.starmousov@gmail.com>
2023-08-10 00:36:10 +0500

    feat: add test for validator(codeium)

--- src/utils/validator/validator.test.ts
+++ src/utils/validator/validator.test.ts
@@ -1,8 +1,37 @@
 import ValidatorUtil from "./validator";
 
 describe("ValidatorUtil", () => {
-  it("should instantiate", () => {
-    const instance = new ValidatorUtil();
-    expect(instance).toBeInstanceOf(ValidatorUtil);
+  let validatorUtil: ValidatorUtil;
+
+  beforeEach(() => {
+    validatorUtil = new ValidatorUtil();
+  });
+
+  describe("validateEmail", () => {
+    it("should return true for a valid email", () => {
+      const validEmail = "test@example.com";
+      const isValid = validatorUtil.validateEmail(validEmail);
+      expect(isValid).toBe(true);
+    });
+
+    it("should return false for an invalid email", () => {
+      const invalidEmail = "invalidemail";
+      const isValid = validatorUtil.validateEmail(invalidEmail);
+      expect(isValid).toBe(false);
+    });
+  });
+
+  describe("validatePassword", () => {
+    it("should return true for a valid password", () => {
+      const validPassword = "password123";
+      const isValid = validatorUtil.validatePassword(validPassword);
+      expect(isValid).toBe(true);
+    });
+
+    it("should return false for an invalid password", () => {
+      const invalidPassword = "pw";
+      const isValid = validatorUtil.validatePassword(invalidPassword);
+      expect(isValid).toBe(false);
+    });
   });
 });
