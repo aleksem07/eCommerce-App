d931515ea6f4a240d409456b537b093ba97cd456
NikitaStarmoussov <nikta.starmousov@gmail.com>
2023-08-10 01:44:13 +0500

    feat: upgrade validator

--- src/utils/validator/validator.ts
+++ src/utils/validator/validator.ts
@@ -1,29 +1,59 @@
 import * as yup from "../../../node_modules/yup/index";
 
 export default class ValidatorUtil {
-  emailSchema: yup.Schema<string>;
-  passwordSchema: yup.Schema<string>;
+  passwordSchema: yup.ObjectSchema<
+    { password: string },
+    yup.AnyObject,
+    { password: undefined },
+    ""
+  >;
+  emailSchema: yup.ObjectSchema<{ email: string }, yup.AnyObject, { email: undefined }, "">;
+  // passwordSchema: yup.Schema<string>;
 
   constructor() {
-    this.emailSchema = yup.string().email().required();
-    this.passwordSchema = yup.string().min(8).required();
+    this.emailSchema = yup.object().shape({
+      email: yup.string().trim().email("Invalid email address").required("Email is required"),
+    });
+    this.passwordSchema = yup.object().shape({
+      password: yup
+        .string()
+        .min(8, "Password must be at least 8 characters long")
+        .matches(/[A-Z]/, "Password must contain at least one uppercase letter")
+        .matches(/[a-z]/, "Password must contain at least one lowercase letter")
+        .matches(/[0-9]/, "Password must contain at least one digit")
+        .matches(/^[^\s]+$/, "Password must not contain leading or trailing whitespace")
+        .matches(/[^A-Za-z0-9]/, "Password must contain at least one special character")
+        .required("Password is required"),
+    });
   }
 
-  validateEmail(email: string): boolean {
+  validateEmail(email: string) {
     try {
-      this.emailSchema.validateSync(email);
-      return true;
+      this.emailSchema.validateSync({ email });
+      return {
+        result: true,
+        message: "Email is valid",
+      };
     } catch (error) {
-      return false;
+      return {
+        result: false,
+        message: (error as Error).message,
+      };
     }
   }
 
-  validatePassword(password: string): boolean {
+  validatePassword(password: string) {
     try {
-      this.passwordSchema.validateSync(password);
-      return true;
+      this.passwordSchema.validateSync({ password });
+      return {
+        result: true,
+        message: "Password is valid",
+      };
     } catch (error) {
-      return false;
+      return {
+        result: false,
+        message: (error as Error).message,
+      };
     }
   }
 }
