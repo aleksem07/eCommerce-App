2167a19e37d7f45a5a3189a6293b9caa14b54a4d
johngaalt <rajon777666@gmail.com>
2023-08-11 14:58:34 +0300

    feat: basic layout

--- src/components/header/header.view.ts
+++ src/components/header/header.view.ts
@@ -1,14 +1,53 @@
 import { ViewBuilder } from "@Interfaces/view-builder";
 
 export default class HeaderView extends ViewBuilder {
-  element: HTMLElement;
+  private element: HTMLElement;
 
   constructor() {
     super();
-    this.element = this.createElement("div");
+    this.element = this.createNavbar();
+  }
+
+  private createNavbar(): HTMLElement {
+    const navbar = this.createElement("nav", {
+      classes: ["navbar", "bg-dark", "border-bottom", "border-body"],
+    });
+    navbar.dataset.bsTheme = "dark";
+
+    const container = this.createElement("div", {
+      classes: ["container-fluid"],
+    });
+
+    const links = this.createLinks(["Главная", "О нас", "Услуги", "Контакты"]);
+    container.appendChild(links);
+
+    navbar.appendChild(container);
+    return navbar;
+  }
+
+  private createLinks(linkTexts: string[]): HTMLElement {
+    const linksContainer = this.createElement("div", {
+      classes: ["navbar-nav"],
+    });
+
+    linkTexts.forEach((text) => {
+      const link = this.createLink(text);
+      linksContainer.appendChild(link);
+    });
+
+    return linksContainer;
+  }
+
+  private createLink(text: string): HTMLElement {
+    const link = this.createElement<HTMLLinkElement>("a", {
+      classes: ["nav-link", "text-white"],
+    });
+    link.href = "#";
+    link.textContent = text;
+    return link;
   }
 
   render() {
-    this.appendTo("", this.element);
+    document.body.prepend(this.element);
   }
 }
