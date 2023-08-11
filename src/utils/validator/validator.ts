6ae589be75139da7aa5eb66c762faf3c42f7a129
NikitaStarmoussov <nikta.starmousov@gmail.com>
2023-08-11 13:48:01 +0500

    fix: use yup for email validation

--- src/utils/validator/validator.ts
+++ src/utils/validator/validator.ts
@@ -1,9 +1,11 @@
 import * as yup from "yup";
-import { PasswordValidationSchema, ValidationResult } from "./validator.types";
+import { ValidationSchema, ValidationResult } from "./validator.types";
 export default class ValidatorUtil {
-  passwordSchema: PasswordValidationSchema;
+  passwordSchema: ValidationSchema;
+  emailSchema: ValidationSchema;
 
   constructor() {
+    this.emailSchema = yup.string().trim().email().required().strict(true).defined();
     this.passwordSchema = yup
       .string()
       .min(8, "Password must be at least 8 characters long")
@@ -14,43 +16,25 @@ export default class ValidatorUtil {
       .matches(/[^A-Za-z0-9]/, "Password must contain at least one special character")
       .required("Password is required");
   }
-  validateEmail(email: string): ValidationResult {
-    const [emailname, domain] = email.split("@");
-    const emailRegex = /^\S+@\S+\.\S+$/;
-    const invalidAnswer: ValidationResult = { isValid: false, message: "" };
-    if (!emailname) {
-      invalidAnswer.message = "Email address is missing username.";
-      return invalidAnswer;
-    }
-    if (!email.includes("@")) {
-      invalidAnswer.message = "Email address should contain an '@' symbol.";
-      return invalidAnswer;
-    }
-    if (!domain) {
-      invalidAnswer.message = "Email address is missing domain name.";
-      return invalidAnswer;
-    }
-    if (!email.includes(".")) {
-      invalidAnswer.message = "Email address should contain a '.' symbol.";
-      return invalidAnswer;
-    }
-    if (email.trim() !== email) {
-      invalidAnswer.message = "Email address should not contain leading or trailing whitespace.";
-      return invalidAnswer;
-    }
-    if (!emailRegex.test(email)) {
-      invalidAnswer.message = "Email address is not properly formatted.";
-      return invalidAnswer;
-    }
-    return {
-      isValid: true,
-      message: " ",
-    };
-  }
 
   validatePassword(password: string): ValidationResult {
     try {
-      this.passwordSchema.validateSync({ password });
+      this.passwordSchema.validateSync(password);
+      return {
+        isValid: true,
+        message: " ",
+      };
+    } catch (error) {
+      return {
+        isValid: false,
+        message: (error as Error).message,
+      };
+    }
+  }
+
+  validateEmail(email: string): ValidationResult {
+    try {
+      this.emailSchema.validateSync(email);
       return {
         isValid: true,
         message: " ",
--- src/utils/validator/validator.types.ts
+++ src/utils/validator/validator.types.ts
@@ -1,5 +1,5 @@
 import * as yup from "../../../node_modules/yup/index";
-export type PasswordValidationSchema = yup.StringSchema<string>;
+export type ValidationSchema = yup.StringSchema<string>;
 
 export interface ValidationResult {
   isValid: boolean;
