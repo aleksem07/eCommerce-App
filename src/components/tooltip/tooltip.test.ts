73ebcedf0e28ca908732e7d5ff952cdadb93fe45
aleksem07 <aleksem07@gmail.com>
2023-08-13 15:11:19 +0300

    feat: add test tooltip

--- src/components/tooltip/tooltip.test.ts
+++ src/components/tooltip/tooltip.test.ts
@@ -1,8 +1,35 @@
-import NotificationHandlerUtil from "./tooltip";
+import Tooltip from "./tooltip";
+
+jest.mock("bootstrap", () => ({
+  Popover: jest.fn().mockImplementation(() => ({
+    show: jest.fn(),
+    dispose: jest.fn(),
+  })),
+}));
 
 describe("ErrorHandlingUtil", () => {
   it("should instantiate", () => {
-    const instance = new NotificationHandlerUtil();
-    expect(instance).toBeInstanceOf(NotificationHandlerUtil);
+    const instance = new Tooltip();
+    expect(instance).toBeInstanceOf(Tooltip);
+  });
+
+  it("should call showPopover method on success", () => {
+    const instance = new Tooltip();
+    const element = document.createElement("div");
+    const successMessage = "Success message";
+    const result = { success: true };
+    instance.showPopover = jest.fn();
+    instance.init(element, result, successMessage);
+    expect(instance.showPopover).toHaveBeenCalledWith(element, "Success", successMessage, 1500);
+  });
+
+  it("should call showPopover method on error", () => {
+    const instance = new Tooltip();
+    const element = document.createElement("div");
+    const errorMessage = "Error message";
+    const result = { success: false, error: errorMessage };
+    instance.showPopover = jest.fn();
+    instance.init(element, result, "");
+    expect(instance.showPopover).toHaveBeenCalledWith(element, "Error", errorMessage, 3500);
   });
 });
