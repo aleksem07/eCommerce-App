f35aea3ac1a70cf1f84db8c211419b254ff6d9e7
aleksem07 <aleksem07@gmail.com>
2023-08-15 03:57:30 +0300

    fix: remove duplicate label titles

--- src/components/login-form/login-form.view.ts
+++ src/components/login-form/login-form.view.ts
@@ -49,6 +49,11 @@ export default class LoginFormView extends ViewBuilder {
   }
 
   private createEmailWrapper() {
+    const existEmailTitle = this.form.querySelector("#login-email-wrapper");
+
+    if (existEmailTitle) {
+      this.form.removeChild(existEmailTitle);
+    }
     const emailWrapper = this.createElement("div", {
       id: "login-email-wrapper",
       classes: ["row"],
@@ -65,6 +70,11 @@ export default class LoginFormView extends ViewBuilder {
   }
 
   private createPasswordWrapper() {
+    const existPasswordTitle = this.form.querySelector("#password-wrapper");
+
+    if (existPasswordTitle) {
+      this.form.removeChild(existPasswordTitle);
+    }
     const passwordWrapper = this.createElement("div", {
       id: "password-wrapper",
       classes: ["row", "mb-3"],
@@ -80,6 +90,11 @@ export default class LoginFormView extends ViewBuilder {
   }
 
   private createCheckbox() {
+    const existCheckboxTitle = this.form.querySelector("#checkbox-wrapper");
+
+    if (existCheckboxTitle) {
+      this.form.removeChild(existCheckboxTitle);
+    }
     const checkboxWrapper = this.createElement("div", {
       id: "checkbox-wrapper",
       classes: ["mb-3", "form-check"],
