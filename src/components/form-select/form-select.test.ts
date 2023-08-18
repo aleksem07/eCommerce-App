8a7d438a00ff058a800109da0badab4519dc8c29
johngaalt <rajon777666@gmail.com>
2023-08-18 18:10:04 +0300

    test: FormSelectComponent should instantiate

--- src/components/form-select/form-select.test.ts
+++ src/components/form-select/form-select.test.ts
@@ -2,7 +2,16 @@ import FormSelectComponent from "./form-select";
 
 describe("FormSelectComponent", () => {
   it("should instantiate", () => {
-    const instance = new FormSelectComponent();
+    const instance = new FormSelectComponent({
+      formName: "form",
+      inputName: "input",
+      labelText: "Label",
+      helpText: "Help",
+      options: [
+        { label: "Option 1", value: "1" },
+        { label: "Option 2", value: "2" },
+      ],
+    });
     expect(instance).toBeInstanceOf(FormSelectComponent);
   });
 });
