7822031d543fa3c7c615c2edf761048615184ac7
johngaalt <rajon777666@gmail.com>
2023-08-20 14:41:26 +0300

    chore: format

--- src/components/link/link.test.ts
+++ src/components/link/link.test.ts
@@ -2,7 +2,10 @@ import LinkComponent from "./link";
 
 describe("LinkComponent", () => {
   it("should instantiate", () => {
-    const instance = new LinkComponent();
+    const instance = new LinkComponent({
+      href: "href",
+      text: "text",
+    });
     expect(instance).toBeInstanceOf(LinkComponent);
   });
 });
