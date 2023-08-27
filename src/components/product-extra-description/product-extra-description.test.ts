dfff1a893bb1ee267bc9715cd132f0bee4f3942b
johngaalt <rajon777666@gmail.com>
2023-08-27 15:06:37 +0300

    fix: test

--- src/components/product-extra-description/product-extra-description.test.ts
+++ src/components/product-extra-description/product-extra-description.test.ts
@@ -2,7 +2,11 @@ import ProductExtraDescriptionComponent from "./product-extra-description";
 
 describe("ProductExtraDescriptionComponent", () => {
   it("should instantiate", () => {
-    const instance = new ProductExtraDescriptionComponent();
+    const instance = new ProductExtraDescriptionComponent({
+      title: "test",
+      content: "test",
+      list: ["test"],
+    });
     expect(instance).toBeInstanceOf(ProductExtraDescriptionComponent);
   });
 });
