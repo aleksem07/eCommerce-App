6a1e89605ec8402c8f8349502e0207a97f6848bc
johngaalt <rajon777666@gmail.com>
2023-08-28 14:42:19 +0300

    feat: basic layout for swiper

--- src/components/product-slider/product-slider.ts
+++ src/components/product-slider/product-slider.ts
@@ -3,8 +3,8 @@ import ProductSliderView from "./product-slider.view";
 export default class ProductSliderComponent {
   private view: ProductSliderView;
 
-  constructor() {
-    this.view = new ProductSliderView();
+  constructor(images: string[]) {
+    this.view = new ProductSliderView(images);
   }
 
   init() {
--- src/components/product-slider/product-slider.view.ts
+++ src/components/product-slider/product-slider.view.ts
@@ -1,14 +1,62 @@
 import { ViewBuilder } from "@Interfaces/view-builder";
+import Swiper from "swiper";
 
 export default class ProductSliderView extends ViewBuilder {
-  private element: HTMLElement;
+  private swiperContainer: HTMLDivElement;
+  private swiperWrapper: HTMLDivElement;
+  private swiper: Swiper | null = null;
+  private images: string[] = [];
 
-  constructor() {
+  constructor(images: string[]) {
     super();
-    this.element = this.createElement("div");
+    this.images = images;
+    this.swiperContainer = this.createSwiperContainer();
+    this.swiperWrapper = this.createSwiperWrapper();
   }
 
-  render() {
-    return this.element;
+  createSwiperContainer(): HTMLDivElement {
+    const container = this.createElement<HTMLDivElement>("div", {
+      classes: ["swiper-container"],
+    });
+
+    return container;
+  }
+
+  createSwiperWrapper(): HTMLDivElement {
+    const wrapper = this.createElement<HTMLDivElement>("div", {
+      classes: ["swiper-wrapper"],
+    });
+
+    return wrapper;
+  }
+
+  createSwiperSlide(imageUrl: string): HTMLDivElement {
+    const slide = this.createElement<HTMLDivElement>("div", {
+      classes: ["swiper-slide"],
+    });
+    const img = this.createElement<HTMLImageElement>("img");
+    img.src = imageUrl;
+    slide.appendChild(img);
+
+    return slide;
+  }
+
+  render(): HTMLElement {
+    this.swiperWrapper.innerHTML = "";
+
+    this.images.forEach((imageUrl) => {
+      const slide = this.createSwiperSlide(imageUrl);
+      this.swiperWrapper.appendChild(slide);
+    });
+
+    this.swiperContainer.appendChild(this.swiperWrapper);
+
+    if (!this.swiper) {
+      this.swiper = new Swiper(".swiper-container");
+    } else {
+      this.swiper.update();
+    }
+
+    return this.swiperContainer;
   }
 }
