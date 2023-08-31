bd5fe226b49454d14df2a40722f320712a8232bf
johngaalt <rajon777666@gmail.com>
2023-08-31 10:48:40 +0300

    test: should render products

--- src/components/product-list/product-list.test.ts
+++ src/components/product-list/product-list.test.ts
@@ -5,4 +5,45 @@ describe("ProductListComponent", () => {
     const instance = new ProductListComponent();
     expect(instance).toBeInstanceOf(ProductListComponent);
   });
+
+  it("should render products", () => {
+    const instance = new ProductListComponent();
+
+    const productListEl = instance.init([
+      {
+        id: "1",
+        title: "test",
+        description: "test",
+        images: ["test"],
+        price: {
+          value: 1,
+          currencyCode: "USD",
+        },
+      },
+      {
+        id: "2",
+        title: "test",
+        description: "test",
+        images: ["test"],
+        price: {
+          value: 1,
+          currencyCode: "USD",
+        },
+      },
+      {
+        id: "3",
+        title: "test",
+        description: "test",
+        images: ["test"],
+        price: {
+          value: 1,
+          currencyCode: "USD",
+        },
+      },
+    ]);
+
+    const productCards = productListEl.querySelectorAll(".col-12");
+
+    expect(productCards).toHaveLength(3);
+  });
 });
