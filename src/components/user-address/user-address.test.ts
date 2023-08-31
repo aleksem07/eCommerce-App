a9c1c09e2538d280310c8e4b1ab89c0a8fe73054
johngaalt <rajon777666@gmail.com>
2023-08-31 17:04:04 +0300

    fix: test

--- src/components/user-address/user-address.test.ts
+++ src/components/user-address/user-address.test.ts
@@ -2,7 +2,7 @@ import UserAddressComponent from "./user-address";
 
 describe("UserAddressComponent", () => {
   it("should instantiate", () => {
-    const instance = new UserAddressComponent();
+    const instance = new UserAddressComponent("test");
     expect(instance).toBeInstanceOf(UserAddressComponent);
   });
 });
