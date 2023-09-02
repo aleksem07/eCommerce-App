0eb6023932a8ded80c6da96654925a1033bc751e
aleksem07 <aleksem07@gmail.com>
2023-09-02 05:00:02 +0300

    feat: add test

--- src/components/sort/sort.test.ts
+++ src/components/sort/sort.test.ts
@@ -1,8 +1,21 @@
 import SortComponent from "./sort";
+import SortView from "./sort.view";
 
 describe("SortComponent", () => {
   it("should instantiate", () => {
     const instance = new SortComponent();
     expect(instance).toBeInstanceOf(SortComponent);
+  });
+  it("should call SortView.render with the correct arguments when init is called", () => {
+    const instance = new SortComponent();
+    const fakeSelectElement = document.createElement("select");
+    instance["sortByInput"].init = jest.fn().mockReturnValue(fakeSelectElement);
+    const fakeRenderedElement = document.createElement("div");
+    SortView.prototype.render = jest.fn().mockReturnValue(fakeRenderedElement);
+
+    const result = instance.init();
+
+    expect(SortView.prototype.render).toHaveBeenCalledWith(fakeSelectElement);
+    expect(result).toEqual(fakeRenderedElement);
   });
 });
