ff58b50aa0771aef6b208662abfddb3d4a0f78f0
johngaalt <rajon777666@gmail.com>
2023-09-03 14:04:34 +0300

    test: should render text, should render address

--- src/components/user-data/user-data.test.ts
+++ src/components/user-data/user-data.test.ts
@@ -18,4 +18,55 @@ describe("UserDataComponent", () => {
     });
     expect(instance).toBeInstanceOf(UserDataComponent);
   });
+
+  it("should not render billing address when it is not provided", () => {
+    const instance = new UserDataComponent({
+      id: "1",
+      firstName: "John",
+      lastName: "Doe",
+      email: "a@b.com",
+      dateOfBirth: "2000-01-01",
+      shippingAddress: {
+        country: "US",
+        city: "New York",
+        streetName: "123 Main St",
+        postalCode: "12345",
+        isDefaultAddress: true,
+      },
+    });
+
+    const element = instance.init();
+
+    const billingAddressInputs = element.querySelectorAll("input[id^=user-data-billing-address]");
+    expect(billingAddressInputs).toHaveLength(0);
+  });
+
+  it("should render billing address when it is provided", () => {
+    const instance = new UserDataComponent({
+      id: "1",
+      firstName: "John",
+      lastName: "Doe",
+      email: "a@b.com",
+      dateOfBirth: "2000-01-01",
+      shippingAddress: {
+        country: "US",
+        city: "New York",
+        streetName: "123 Main St",
+        postalCode: "12345",
+        isDefaultAddress: true,
+      },
+      billingAddress: {
+        country: "US",
+        city: "Miami",
+        streetName: "1234 Main St",
+        postalCode: "12346",
+        isDefaultAddress: true,
+      },
+    });
+
+    const element = instance.init();
+
+    const billingAddressInputs = element.querySelectorAll("input[id^=user-data-billing-address]");
+    expect(billingAddressInputs).toHaveLength(4);
+  });
 });
--- src/components/user-menu/user-menu.test.ts
+++ src/components/user-menu/user-menu.test.ts
@@ -5,4 +5,19 @@ describe("UserMenuComponent", () => {
     const instance = new UserMenuComponent("Full Name", "Email@Address.com");
     expect(instance).toBeInstanceOf(UserMenuComponent);
   });
+
+  it.each([
+    { text: "Full Name" },
+    { text: "Email@Address.com" },
+    { text: "My profile" },
+    { text: "Sign out" },
+  ])("should render $text", ({ text }) => {
+    const instance = new UserMenuComponent("Full Name", "Email@Address.com");
+
+    const list = instance.init();
+
+    const allElements = Array.from(list.querySelectorAll("*"));
+    const element = allElements?.find((item) => item.textContent === text);
+    expect(element).toBeTruthy();
+  });
 });
