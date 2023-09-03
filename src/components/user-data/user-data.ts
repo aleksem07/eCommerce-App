3fc1af5a6aa70caf3ba62621a6dc3dc42e1a1a33
johngaalt <rajon777666@gmail.com>
2023-09-03 17:26:58 +0300

    feat: edit button listener, save button listener

--- src/components/user-data/user-data.ts
+++ src/components/user-data/user-data.ts
@@ -6,33 +6,54 @@ import { Customer } from "@Services/customer/customer.types";
 
 export default class UserDataComponent {
   private view: UserDataView;
-  private userInfo: UserInfoComponent;
-  private userPassword: UserPasswordComponent;
-  private userShippingAddress: UserAddressComponent;
+  private userInfo!: UserInfoComponent;
+  private userPassword!: UserPasswordComponent;
+  private userShippingAddress!: UserAddressComponent;
   private userBillingAddress?: UserAddressComponent;
   private formName = "user-data";
   private isEditMode = false;
+  private customer: Customer;
 
   constructor(customer: Customer) {
+    this.customer = customer;
     this.view = new UserDataView();
-    this.userInfo = new UserInfoComponent(this.formName, customer, this.isEditMode);
+    this.instantiateComponents();
+
+    this.view.editButtonListener(this.editButtonHandler.bind(this));
+    this.view.saveButtonListener(this.saveButtonHandler.bind(this));
+  }
+
+  private instantiateComponents() {
+    this.userInfo = new UserInfoComponent(this.formName, this.customer, this.isEditMode);
     this.userPassword = new UserPasswordComponent(this.formName, this.isEditMode);
 
     this.userShippingAddress = new UserAddressComponent({
       header: "Shipping Address",
       formName: this.formName,
-      address: customer.shippingAddress,
+      address: this.customer.shippingAddress,
       isEditMode: this.isEditMode,
     });
 
-    if (customer.billingAddress) {
+    if (this.customer.billingAddress) {
       this.userBillingAddress = new UserAddressComponent({
         header: "Billing Address",
         formName: this.formName,
-        address: customer.billingAddress,
+        address: this.customer.billingAddress,
         isEditMode: this.isEditMode,
       });
     }
+  }
+
+  editButtonHandler() {
+    this.isEditMode = true;
+    this.instantiateComponents();
+    this.init();
+  }
+
+  saveButtonHandler() {
+    this.isEditMode = false;
+    this.instantiateComponents();
+    this.init();
   }
 
   init() {
@@ -41,6 +62,12 @@ export default class UserDataComponent {
     const userInfo = this.userInfo.init();
     const userPassword = this.userPassword.init();
 
-    return this.view.render({ userInfo, userPassword, userShippingAddress, userBillingAddress });
+    return this.view.render({
+      userInfo,
+      userPassword,
+      userShippingAddress,
+      userBillingAddress,
+      isEditMode: this.isEditMode,
+    });
   }
 }
--- src/components/user-data/user-data.view.ts
+++ src/components/user-data/user-data.view.ts
@@ -3,17 +3,44 @@ import { ViewBuilder } from "@Interfaces/view-builder";
 export default class UserDataView extends ViewBuilder {
   private element: HTMLDivElement;
   private header: HTMLHeadingElement;
-  private button: HTMLButtonElement;
+  private saveButton: HTMLButtonElement;
+  private editButton: HTMLButtonElement;
 
   constructor() {
     super();
-    this.header = this.createElement("h1");
+    this.header = this.createElement("h1", {
+      classes: ["d-flex", "align-items-center", "justify-content-between"],
+    });
     this.header.textContent = "My profile";
-
     this.element = this.createElement("div");
 
-    this.button = this.createElement("button", { classes: ["btn", "btn-primary", "mt-4"] });
-    this.button.textContent = "Save changes";
+    this.saveButton = this.createElement("button", { classes: ["btn", "btn-primary"] });
+    this.saveButton.textContent = "Save";
+    this.header.append(this.saveButton);
+
+    this.editButton = this.createElement<HTMLButtonElement>("button", {
+      classes: ["btn", "btn-secondary"],
+    });
+    this.editButton.textContent = "Edit";
+    this.header.append(this.editButton);
+  }
+
+  private toggleButtons(isEditMode: boolean) {
+    if (isEditMode) {
+      this.editButton.classList.add("d-none");
+      this.saveButton.classList.remove("d-none");
+    } else {
+      this.editButton.classList.remove("d-none");
+      this.saveButton.classList.add("d-none");
+    }
+  }
+
+  editButtonListener(handler: () => void) {
+    this.editButton?.addEventListener("click", handler);
+  }
+
+  saveButtonListener(handler: () => void) {
+    this.saveButton?.addEventListener("click", handler);
   }
 
   render({
@@ -21,18 +48,22 @@ export default class UserDataView extends ViewBuilder {
     userPassword,
     userShippingAddress,
     userBillingAddress,
+    isEditMode,
   }: {
     userInfo: HTMLElement;
     userPassword: HTMLElement;
     userShippingAddress: HTMLElement;
     userBillingAddress?: HTMLElement;
+    isEditMode: boolean;
   }) {
+    this.element.innerHTML = "";
+    this.toggleButtons(isEditMode);
     const elements = [this.header, userInfo, userPassword, userShippingAddress];
 
     if (userBillingAddress) {
       elements.push(userBillingAddress);
     }
-    this.element.append(...elements, this.button);
+    this.element.append(...elements);
 
     return this.element;
   }
