e05a469df7b9343fb7a37ce674d9941d630c81fe
NikitaStarmoussov <nikta.starmousov@gmail.com>
2023-09-04 19:29:24 +0500

    Revert "feat: add working breadcrumbs"
    This reverts commit 31ca1caf158c3af54e529d43b0a01281ef65e675.

--- src/components/bread-crumbs/bread-crumbs.test.ts
+++
@@ -1,8 +0,0 @@
-import BreadCrumbsComponent from "./bread-crumbs";
-
-describe("BreadCrumbsComponent", () => {
-  it("should instantiate", () => {
-    const instance = new BreadCrumbsComponent();
-    expect(instance).toBeInstanceOf(BreadCrumbsComponent);
-  });
-});
--- src/pages/catalog/catalog.ts
+++ src/pages/catalog/catalog.ts
@@ -10,11 +10,9 @@ import { FilterAttributeType } from "./catalog.types";
 import RouterService from "@Services/router/router";
 import { Routes } from "@Services/router/router.types";
 import { NotificationVariant } from "@Components/notification/notification.types";
-import BreadCrumbsComponent from "@Components/bread-crumbs/bread-crumbs";
 
 export default class CatalogPage {
   private view: CatalogView;
-  private breadcrumbs: BreadCrumbsComponent;
   private productService: ProductService;
   private productListComponent: ProductListComponent;
   private filter: FilterComponent;
@@ -31,7 +29,6 @@ export default class CatalogPage {
 
   constructor() {
     this.view = new CatalogView();
-    this.breadcrumbs = new BreadCrumbsComponent();
     this.productService = new ProductService();
     this.productListComponent = new ProductListComponent();
     this.filter = new FilterComponent();
@@ -194,7 +191,6 @@ export default class CatalogPage {
     this.view.displayToolbar(this.sort.init());
     this.view.displaySidebar(this.filter.init());
     this.checkCategoryExists();
-    this.breadcrumbs.init();
     this.view.render();
   }
 }
