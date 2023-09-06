0f6917ee0426d90a4912336c58245f63634fbdb8
johngaalt <rajon777666@gmail.com>
2023-09-06 20:28:51 +0300

    feat: home link

--- src/components/cart-list/cart-list.view.ts
+++ src/components/cart-list/cart-list.view.ts
@@ -1,8 +1,10 @@
 import { ViewBuilder } from "@Interfaces/view-builder";
+import { Routes } from "@Services/router/router.types";
 
 export default class CartListView extends ViewBuilder {
   private element: HTMLElement;
   private header: HTMLHeadingElement;
+  private homeLink: HTMLLinkElement;
 
   constructor() {
     super();
@@ -11,13 +13,26 @@ export default class CartListView extends ViewBuilder {
     });
 
     this.header = this.createHeaderElement();
+    this.homeLink = this.createHomeLink();
   }
 
   createHeaderElement(): HTMLHeadingElement {
-    const header = this.createElement<HTMLHeadingElement>("h3");
+    const header = this.createElement<HTMLHeadingElement>("h1", {
+      classes: ["d-flex", "align-items-center", "justify-content-between"],
+    });
     header.textContent = "Your cart";
 
     return header;
+  }
+
+  createHomeLink(): HTMLLinkElement {
+    const link = this.createElement<HTMLLinkElement>("a", {
+      classes: ["fs-2", "link-primary", "link-offset-2"],
+    });
+    link.textContent = "Home";
+    link.href = Routes.MAIN;
+
+    return link;
   }
 
   private createColumn(element: HTMLElement) {
@@ -32,6 +47,7 @@ export default class CartListView extends ViewBuilder {
   render(...elements: HTMLElement[]) {
     const columns = elements.map((element) => this.createColumn(element));
     this.element.innerHTML = "";
+    this.header.append(this.homeLink);
     this.element.append(...columns, this.header);
 
     return this.element;
