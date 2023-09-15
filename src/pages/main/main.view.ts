e71a44a03178c2476e0defa1915962f682bccfe6
NikitaStarmoussov <nikta.starmousov@gmail.com>
2023-09-15 16:40:08 +0500

    feat: add promo code to main page

--- src/pages/main/main.view.ts
+++ src/pages/main/main.view.ts
@@ -3,6 +3,7 @@ import { ViewBuilder } from "@Interfaces/view-builder";
 export default class MainView extends ViewBuilder {
   container: HTMLElement;
   heading: HTMLHeadingElement;
+  promoCode: HTMLElement;
   constructor() {
     super();
     this.container = this.createElement("div", {
@@ -14,8 +15,12 @@ export default class MainView extends ViewBuilder {
       classes: ["h3", "mb-3", "fw-normal", "text-center", "py-5"],
     });
     this.heading.textContent = "Main Page";
+    this.promoCode = this.createElement("h2", {
+      classes: ["h3", "mb-3", "fw-normal", "text-center", "py-5"],
+    });
+    this.promoCode.textContent = 'Promo Code: "off10"';
 
-    this.container.append(this.heading);
+    this.container.append(this.heading, this.promoCode);
   }
 
   render() {
