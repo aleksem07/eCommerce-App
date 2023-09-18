49a1dc75819e73447aad99930414eeb388cea3c3
NikitaStarmoussov <nikta.starmousov@gmail.com>
2023-09-18 12:36:05 +0500

    feat: add test

--- src/components/promo-code/promo-code.test.ts
+++ src/components/promo-code/promo-code.test.ts
@@ -1,8 +1,37 @@
 import PromoCodeComponent from "./promo-code";
+import FormControlComponent from "@Components/form-control/form-control";
+import CartService from "@Services/cart/cart";
+
+// Mock dependencies
+jest.mock("@Components/form-control/form-control");
+jest.mock("@Services/cart/cart");
 
 describe("PromoCodeComponent", () => {
-  it("should instantiate", () => {
-    const instance = new PromoCodeComponent();
-    expect(instance).toBeInstanceOf(PromoCodeComponent);
+  let promoCodeComponent: PromoCodeComponent;
+
+  beforeEach(() => {
+    promoCodeComponent = new PromoCodeComponent();
+  });
+
+  it("should create an instance of PromoCodeComponent", () => {
+    expect(promoCodeComponent).toBeInstanceOf(PromoCodeComponent);
+  });
+
+  it("should create an instance of FormControlComponent with the correct configuration", () => {
+    expect(FormControlComponent).toHaveBeenCalledWith(
+      expect.objectContaining({
+        formName: "promo-code",
+        inputName: "promo-code",
+        labelText: "Apply a promo code",
+        placeholderText: "Enter promo code",
+        type: "text",
+        classes: expect.arrayContaining(["form-control", "border-0", "p-0"]),
+        disabled: false,
+      })
+    );
+  });
+
+  it("should create an instance of CartService", () => {
+    expect(CartService).toHaveBeenCalled();
   });
 });
